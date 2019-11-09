a2a9785402fd5a2fbe678ae2642c29a9b8268323
Minh Bui <tranduyminh.bui@gmail.com>
2019-11-09 19:34:55 +1030

    playback bar

--- index.php
+++ index.php
@@ -18,9 +18,79 @@ if (isset($_SESSION['userLoggedIn'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="assets/css/index.css">
   <title>Document</title>
 </head>
 <body>
-  hello
+
+  <div id="nowPlayingBarContainer">
+    <div id="nowPlayingBar">
+
+      <div id="nowPlayingLeft">
+        <div class="content">
+          <span class="albumLink">
+            <img class="albumArtwork" src="https://source.unsplash.com/random/100x100" alt="">
+          </span>
+          <div class="trackInfo">
+            <span class="trackName">Piano Man</span>
+            <span class="artistName">Billy Joel</span>
+          </div>
+        </div>
+      </div>
+
+      <div id="nowPlayingCenter">
+        <div class="content playerControls">
+          <div class="buttons">
+            <button class="controlButton shuffle" title="Shuffle button">
+              <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+            </button>
+            <button class="controlButton prev" title="Previous button">
+              <img src="assets/images/icons/prev.png" alt="Previous">
+            </button>
+            <button class="controlButton play" title="Play button">
+              <img src="assets/images/icons/play.png" alt="Play">
+            </button>
+            <button class="controlButton pause" title="Pause button" style="display: none">
+              <img src="assets/images/icons/pause.png" alt="Pause">
+            </button>
+            <button class="controlButton next" title="Next button">
+              <img src="assets/images/icons/next.png" alt="Next">
+            </button>
+            <button class="controlButton repeat" title="Repeat button">
+              <img src="assets/images/icons/repeat.png" alt="Repeat">
+            </button>
+          </div>
+
+          <div class="playbackBar">
+            <span class="progressTime current">0.00</span>
+
+            <div class="progressBar">
+              <div class="progressBarBg">
+                <div class="progress"></div>
+              </div>
+            </div>
+
+            <span class="progressTime remaining">0.00</span>
+          </div>
+
+        </div>
+      </div>
+
+      <div id="nowPlayingRight">
+        <div class="volumeBar">
+          <button class="controlButton volume" title="Volume">
+            <img src="assets/images/icons/volume.png" alt="Volume">
+
+          </button>
+          <div class="progressBar">
+              <div class="progressBarBg">
+                <div class="progress"></div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </body>
 </html>
